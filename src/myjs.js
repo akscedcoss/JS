666686fa85473c40c242ecf5b941108cb4a93b4b
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-02-05 14:22:11 +0530

    Add New Properties to a JavaScript Object

--- src/myjs.js
+++ src/myjs.js
@@ -1,10 +1,9 @@
-// Setup
-let myDog = {
-    "name": "Coder",
+const myDog = {
+    "name": "Happy Coder",
     "legs": 4,
     "tails": 1,
     "friends": ["freeCodeCamp Campers"]
   };
   
-  // Only change code below this line
-  myDog.name="Happy Coder";
+  
+  myDog["bark"]="woof"
