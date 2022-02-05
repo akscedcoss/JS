c86fce38268c2f103bf945a64934d24ad2f01511
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-02-05 14:38:25 +0530

    Accessing Nested Objects

--- src/myjs.js
+++ src/myjs.js
@@ -1,23 +1,13 @@
-const myMusic = [
-    {
-      "artist": "Billy Joel",
-      "title": "Piano Man",
-      "release_year": 1973,
-      "formats": [
-        "CD",
-        "8T",
-        "LP"
-      ],
-      "gold": true
-    },
-  {
-        "artist": "Billy Joel",
-      "title": "Piano Man",
-      "release_year": 1973,
-      "formats": [
-        "CD",
-        "8T",
-        "LP"
-      ]
-  }
-  ];
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
