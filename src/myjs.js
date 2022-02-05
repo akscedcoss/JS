f64fe8e9e2e7439263fb116f7c2db12c6ef9e9f3
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-02-05 14:12:29 +0530

    Build JavaScript Objects

--- src/myjs.js
+++ src/myjs.js
@@ -1,26 +1,9 @@
-let count = 0;
-
-function cc(card) {
-  switch(card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    case 10:
-    case 'J':
-    case 'Q':
-    case 'K':
-    case 'A':
-      count--;
-  }
-  if(count > 0) {
-    return count + " Bet";
-  } else {
-    return count + " Hold";
-  }
-}
-
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+const myDog = {
+    // Only change code below this line
+  name:"billu",
+  legs:4,
+  tails:1,
+  friends:["aks","hone"]
+  
+    // Only change code above this line
+  };
