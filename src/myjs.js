2187f3dcc02afd6e506ba164960345fdbc036cda
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-02-05 10:24:53 +0530

    Explore Differences Between the var and let Keywords

--- src/myjs.js
+++ src/myjs.js
@@ -1,9 +1,2 @@
-// Variable declarations
-var studlyCapVar=10;
-var properCamelCase;
-var titleCaseOver;
-
-// Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
+let catName = "Oliver";
+let catSound = "Meow!";
