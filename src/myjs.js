9a36480647496c98232d31c788f3611b6f1a0dbd
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-02-05 11:47:47 +0530

    Store Multiple Values in one Variable using JavaScript Arrays

--- src/myjs.js
+++ src/myjs.js
@@ -1,8 +1,2 @@
-const myNoun = "dog";
-const myAdjective = "big";
-const myVerb = "ran";
-const myAdverb = "quickly";
-
 // Only change code below this line
-var wordBlanks = "My " + myAdjective + " and dumb " + myNoun + " has " + myVerb + " very " + myAdverb + " into the street."; // Change this line
-console.log(wordBlanks)
+const myArray = ["ffg",45];
