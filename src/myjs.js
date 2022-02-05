d410ba385bb47f726cfcd9d85c24c8506ab3cdfe
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-02-05 15:00:35 +0530

    Iterate Odd Numbers With a For Loop

--- src/myjs.js
+++ src/myjs.js
@@ -2,8 +2,7 @@
 const myArray = [];
 
 // Only change code below this line
-for(let i=1 ;i<=5;i++)
-{
+for (let i = 1; i < 10; i += 2) {
   myArray.push(i);
 }
-console.log(myArray)
+console.log(myArray);
