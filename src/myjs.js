27812a9a63ff97d52077403b25e49170938cbfbd
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-02-05 12:39:26 +0530

    Comparison with the Less Than Operator

--- src/myjs.js
+++ src/myjs.js
@@ -1,14 +1,15 @@
-function testGreaterOrEqual(val) {
-    if (val >=20) {  // Change this line
-      return "20 or Over";
+function testLessThan(val) {
+    if (val<25) {  // Change this line
+      return "Under 25";
     }
   
-    if (val>=10) {  // Change this line
-      return "10 or Over";
+    if (val<55) {  // Change this line
+      return "Under 55";
     }
-  if(val<10){
-    return "Less than 10";
-  }
-  }
+    if(val>=55)
+    {
   
-  testGreaterOrEqual(10);
+    return "55 or Over";
+  }
+  }
+  testLessThan(10);
