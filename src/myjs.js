1892ee71455be9d8601149378d78f04270d783ce
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-02-05 12:36:46 +0530

    Comparison with the Greater Than Operator

--- src/myjs.js
+++ src/myjs.js
@@ -1,9 +1,14 @@
-// Setup
-function testStrictNotEqual(val) {
-    if (val!==17) { // Change this line
-      return "Not Equal";
+function testGreaterThan(val) {
+    if (val >100) {  // Change this line
+      return "Over 100";
     }
-    return "Equal";
-  }
   
-  testStrictNotEqual(10);
+    if (val>10) {  // Change this line
+      return "Over 10";
+    }
+  if(val<=10)
+  {
+    return "10 or Under";
+  }
+  }
+  testGreaterThan(10);
